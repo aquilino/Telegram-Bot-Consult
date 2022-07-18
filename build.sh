eb6b53dc0fcd2353553dc746fca690b1dcc1a8c7
Aquilino <hidro23@hotmail.com>
2022-07-18 16:58:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker-compose down && \
 docker build -t h1dr0/bot:amd64 . && \
-docker-compose up -d && \
+docker-compose -f docker-compose.yml -f docker-compose.caddy.yml up -d && \
 docker-compose logs -f
