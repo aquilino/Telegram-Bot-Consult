3e31ab0957d6266026a034a5cb1454e2ad984cf2
Aquilino <hidro23@hotmail.com>
2022-07-18 20:43:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo -e "\t\n[*] ejemplo -> ./build.sh caddy "
+sleep(2);
+
+a = $1
+
 docker-compose down && \
 docker build -t h1dr0/bot:amd64 . && \
-docker-compose -f docker-compose.yml -f docker-compose.caddy.yml up -d && \
+docker-compose -f docker-compose.yml -f docker-compose.$a.yml up -d && \
 docker-compose logs -f
